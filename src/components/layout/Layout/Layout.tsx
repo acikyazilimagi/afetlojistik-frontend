70bae9a49aa0e952f66b798a6979419145d986cd
Tunahan Danış <90687687+tunahandanis@users.noreply.github.com>
2023-02-09 21:07:07 +0300

    convert all inline css

--- src/components/layout/Layout/Layout.tsx
+++ src/components/layout/Layout/Layout.tsx
@@ -3,18 +3,20 @@ import React from 'react'
 import { LayoutProps } from '@pankod/refine-core'
 import { AntdLayout, Grid } from '@pankod/refine-antd'
 
+import styles from './Layout.module.scss'
+
 export const Layout: React.FC<LayoutProps> = ({ children, Sider, Header, Footer, OffLayoutArea }) => {
   const breakpoint = Grid.useBreakpoint()
   return (
-    <AntdLayout style={{ minHeight: '100vh', flexDirection: 'row' }}>
+    <AntdLayout className={styles.layout}>
       {Sider && <Sider />}
       <AntdLayout>
         {Header && <Header />}
         <AntdLayout.Content>
           <div
+            className={styles.layoutContent}
             style={{
-              padding: breakpoint.sm ? 24 : 12,
-              minHeight: 360
+              padding: breakpoint.sm ? 24 : 12
             }}
           >
             {children}
