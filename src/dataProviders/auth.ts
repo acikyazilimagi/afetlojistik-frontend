100e3072910b53d5ac0419b84244d618f39bfe98
Utku Turkoglu <turkoglu.utku@gmail.com>
2023-02-10 11:35:06 +0300

    transform response

--- src/dataProviders/auth.ts
+++ src/dataProviders/auth.ts
@@ -1,8 +1,11 @@
 import { LoginFormType } from 'types/login'
 import { UserLoginResponseType } from 'types/user'
 import { http } from 'services/http'
+import { transformResponseData } from 'utils/http'
 
 export const login = async (data: LoginFormType): Promise<UserLoginResponseType> =>
-  http.post('user/login', {
-    ...data
-  })
+  http
+    .post('user/login', {
+      ...data
+    })
+    .then(transformResponseData)
