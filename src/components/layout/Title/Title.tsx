14454d9fcc39d9a7041ba9ffe9c41e49d7dbef68
Utku Turkoglu <turkoglu.utku@gmail.com>
2023-02-10 21:41:11 +0300

    title fix

--- src/components/layout/Title/Title.tsx
+++ src/components/layout/Title/Title.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { TitleProps } from '@pankod/refine-core'
 import routerProvider from '@pankod/refine-react-router-v6'
 
+import { Typography } from 'antd'
 import styles from './Title.module.scss'
 
 const { Link } = routerProvider
@@ -10,18 +11,10 @@ export const Title: React.FC<TitleProps> = ({ collapsed }) => (
   <Link to='/'>
     {collapsed ? (
       <div className={styles.imageWrapper}>
-        <img
-          src='https://refine.ams3.cdn.digitaloceanspaces.com/logo/refine-mini.svg'
-          alt='Refine'
-          className={styles.wrappedImage}
-        />
+        <Typography.Text className={styles.wrappedImage}>AL</Typography.Text>
       </div>
     ) : (
-      <img
-        src='https://refine.ams3.cdn.digitaloceanspaces.com/logo/refine.svg'
-        alt='Refine'
-        className={styles.soloImage}
-      />
+      <Typography.Text className={styles.wrappedImage}>Afet Lojistik</Typography.Text>
     )}
   </Link>
 )
