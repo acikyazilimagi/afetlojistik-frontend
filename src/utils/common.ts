bcc81e35b8aa0c500bd81f097b74e43da9d6462a
zhunor <ozerorhun@gmail.com>
2023-02-11 15:58:08 +0300

    Fix: turkish character search problem

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,1 +1,10 @@
 export const t = (s: string) => s
+// replace all non turkish characters with english ones
+export const replaceTurkishChars = (s?: string) => {
+  const turkishChars = 'çÇğĞıİöÖşŞüÜ'
+  const englishChars = 'cCgGiIoOsSuU'
+  return s?.replace(/./g, (c) => {
+    const index = turkishChars.indexOf(c)
+    return index > -1 ? englishChars[index] : c
+  })
+}
