31294413f4e46983ff276f748ee0ce28270a5f0a
Orhun Özer <ozerorhun@gmail.com>
2023-02-11 13:10:15 +0300

    Fix: list api url (#22)

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
   return (
     <ColorModeContextProvider>
       <Refine
-        dataProvider={dataProvider(process.env.REACT_APP_BACKEND_URL)}
+        dataProvider={dataProvider('https://test.afetlojistik.com/api')}
         notificationProvider={notificationProvider}
         ReadyPage={ReadyPage}
         catchAll={<ErrorComponent />}
