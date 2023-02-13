a4ff3472702735779c9a0ef11bca46db1bb1a37a
Tunahan Danış <90687687+tunahandanis@users.noreply.github.com>
2023-02-13 15:37:12 +0300

    update district endpoint

--- src/services/district.ts
+++ src/services/district.ts
@@ -3,4 +3,4 @@ import { DistrictType } from 'types/district'
 import { transformResponseData } from 'utils/http'
 
 export const getDistrictList = (cityId: string): Promise<DistrictType[]> =>
-  http.get('location/districts/city', { params: { cityId } }).then(transformResponseData)
+  http.get(`location/cities/${cityId}/districts`).then(transformResponseData)
